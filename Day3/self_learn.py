c812f23c63f931b6a733919d12128e3a5de3ce1a
nidhig631 <nidhig631@gmail.com>
2023-06-08 00:23:45 +0530

    day3_learning

---
+++ Day3/self_learn.py
@@ -0,0 +1,34 @@
+#TUPLES
+# tuples are immutable
+# a = (1,2,3)
+
+#dictionary
+# ipl={"CSK":"Chennai Super King",
+#      "RCB":"Royal challenges banglore",
+#      "MI":"Mumbai Indians"}
+# print(ipl["CSK"])
+# ipl["GT"] = "Gujrat Titans"
+# print(ipl["GT"])
+# ipl["CSK"]="Chennai"
+# print(ipl["CSK"])
+
+# del ipl["CSK"]
+# print(ipl)
+
+
+# ipl = {
+#     "CSK" : {"Name":"Chennai Super Kings","captain":"MSD"},
+#     "MI"  : {"Name":"Mumbai Indians","captain":"Rohit"}
+#     }
+# print(ipl["CSK"]["Name"])
+
+#loops
+num =[1,2,3]
+for i in num:
+    num_sq = num ** num
+    print(num_sq) 
+
+
+
+
+
